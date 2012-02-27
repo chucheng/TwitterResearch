e67ebf098883041e96d93c8330da4e996ea7b187
Chu-Cheng Hsieh <chucheng@gmail.com>
2012-02-26 21:21:33 -0800

    add working dir for chucheng at birch

--- src/Configuration.py
+++ src/Configuration.py
@@ -10,6 +10,7 @@ http://docs.python.org/library/configparser.html
 
 import ConfigParser 
 import socket
+import os
 
 def getConfig():
     """Read TwitterResearch.cfg and dynamically determine base dir by hostname.    
@@ -21,8 +22,18 @@ def getConfig():
         project_base_dir = "/home/chucheng/Projects/github/TwitterResearch/"
     elif hostname == "Shahinshah.local":
         project_base_dir = "/Users/cmoghbel/Code/TwitterResearch/"
-    elif hostname == "shahin-desktop" or hostname == "birch":
+    elif hostname == "shahin-desktop":
         project_base_dir = "/home/cmoghbel/Code/TwitterResearch/"
+    elif hostname == "birch":
+        username = os.getusername();
+        if usename == "chucheng"
+            project_base_dir = "/home/chucheng/projects/github/TwitterResearch/"
+        elif username == "cmoghbel":
+            project_base_dir = "/home/cmoghbel/Code/TwitterResearch/"
+        else:
+            log("Error: (birch) username unknown - " + username)
+            return None
+
     else:
         log("Error: hostname unknown - " + hostname)
         return None
