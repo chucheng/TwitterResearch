725f3413c05a29969b8552b506cc4e11459cc18f
Chris Moghbel <cmoghbel@gmail.com>
2012-02-27 17:55:56 -0800

    Checkpoint: Generating correct data to draw source device graphs.

--- src/a_source_device.py
+++ src/a_source_device.py
@@ -6,46 +6,56 @@ __author__ = 'Chris Moghbel (cmoghbel@cs.ucla.edu)'
 """
 import FileLog
 import Util
+import URLUtil
+import ground_truths
+from ground_truths import DataSet
 
 import os
+import sys
 
 from datetime import datetime
 
-_TWEETFILE_TWEET_ID_INDEX = 0
-_TWEETFILE_USER_ID_INDEX = 1
-_TWEETFILE_TWEET_TEXT_INDEX = 2
-_TWEETFILE_CREATED_AT_INDEX = 3
-_TWEETFILE_RETWEETED_INDEX = 4
-_TWEETFILE_RETWEET_COUNT_INDEX = 5
-_TWEETFILE_ORIGIN_USER_ID_INDEX = 6
-_TWEETFILE_ORIGIN_TWEET_ID_INDEX = 7
-_TWEETFILE_SOURCE_INDEX = 8
-_TWEETFILE_FILTER_WORDS_INDEX = 9
-_TWEETFILE_INSERT_TIMESTAMP_INDEX = 10
+from constants import _TIMEDELTAS_FILE_TWEET_ID_INDEX
+from constants import _TIMEDELTAS_FILE_DELTA_INDEX
 
-_DATA_DIR = '/dfs/birch/tsv'
+from constants import _TWEETFILE_TWEET_ID_INDEX
+from constants import _TWEETFILE_CREATED_AT_INDEX
+from constants import _TWEETFILE_RETWEET_COUNT_INDEX
+from constants import _TWEETFILE_SOURCE_INDEX
+from constants import _TWEETFILE_TWEET_TEXT_INDEX
+
+from constants import _DATETIME_FORMAT
+from constants import _WINDOW_MONTHS
+from constants import _DELTAS
+
+_SIZE_TOP_NEWS = .02
+
 _LOG_FILE = 'a_source_device.log'
 _OUTPUT_DIR = '../data/SourceDevice/'
-_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
-_FULL_SET_MONTHS = ['08', '09', '10', '11', '12', '01']
 
 
-def find_device_counts():
+def find_device_counts(max_delta, deltas, top_news=None, cache=None):
   """Finds the number of tweets by each source device.
 
   Returns:
   Dictionary of source device string to pair of (count, percentage).
   e.g. {'Twitter for iPhone': (1100, 10.0) ...}
   """
+  if top_news and not cache:
+    log('If top news is given, cache must be provided so we can parse urls')
+    exit()
   device_counts = {}
   all_count = 0
   device_counts_original = {}
   original_count = 0
   device_counts_retweets = {}
   retweet_count = 0
-  for month in _FULL_SET_MONTHS:
-    log('Finding device counts for month %s.' % month)
-    dir_name = get_data_dir_name_for(month) 
+  for month in _WINDOW_MONTHS:
+    log('Finding device counts for month %s and delta %s.' % (month, max_delta))
+    if top_news:
+      log('Limiting device counts to tweets with urls in top %s percent of '
+          'ground truths' % int(_SIZE_TOP_NEWS * 100))
+    dir_name = Util.get_data_dir_name_for(month) 
     for filename in os.listdir(dir_name):
       if '.tweet' in filename and 'http_nyti_ms' in filename:
         data_file = '%s/%s' % (dir_name, filename)
@@ -54,26 +64,46 @@ def find_device_counts():
             tokens = line.split('\t')
             created = datetime.strptime(tokens[_TWEETFILE_CREATED_AT_INDEX],
                                         _DATETIME_FORMAT)
-            if is_in_window(created):
-              source_device = tokens[_TWEETFILE_SOURCE_INDEX]
-              retweet = bool(int(tokens[_TWEETFILE_RETWEET_COUNT_INDEX]))
-              all_count += 1
-              if source_device in device_counts:
-                device_counts[source_device] += 1
-              else:
-                device_counts[source_device] = 1
-              if retweet:
-                retweet_count += 1
-                if source_device in device_counts_retweets:
-                  device_counts_retweets[source_device] += 1
-                else:
-                  device_counts_retweets[source_device] = 1
-              else:
-                original_count += 1
-                if source_device in device_counts_original:
-                  device_counts_original[source_device] += 1
-                else:
-                  device_counts_original[source_device] = 1
+            if Util.is_in_window(created):
+              tweet_id = tokens[_TWEETFILE_TWEET_ID_INDEX]
+              # If we don't see the tweet_id in the timedeltas file, we weren't
+              # able to parse a url from the tweet text, so lets ignore it by
+              # setting default delta to sys.maxint
+              delta = sys.maxint
+              if tweet_id in deltas:
+                delta = deltas[tweet_id]
+              if delta < max_delta: 
+                # Check if url is in top news set. If no top news is given,
+                # then it defaults to yes.
+                in_top_news = True
+                if top_news:
+                  in_top_news = False
+                  tweet_text = tokens[_TWEETFILE_TWEET_TEXT_INDEX]
+                  urls = URLUtil.parse_urls(tweet_text, cache)
+                  for url in urls:
+                    if url in top_news:
+                      in_top_news = True
+
+                if in_top_news:
+                  source_device = tokens[_TWEETFILE_SOURCE_INDEX]
+                  retweet = bool(int(tokens[_TWEETFILE_RETWEET_COUNT_INDEX]))
+                  all_count += 1
+                  if source_device in device_counts:
+                    device_counts[source_device] += 1
+                  else:
+                    device_counts[source_device] = 1
+                  if retweet:
+                    retweet_count += 1
+                    if source_device in device_counts_retweets:
+                      device_counts_retweets[source_device] += 1
+                    else:
+                      device_counts_retweets[source_device] = 1
+                  else:
+                    original_count += 1
+                    if source_device in device_counts_original:
+                      device_counts_original[source_device] += 1
+                    else:
+                      device_counts_original[source_device] = 1
 
   for device, count in device_counts_original.items():
     device_total = device_counts[device]
@@ -91,28 +121,19 @@ def find_device_counts():
   return device_counts, device_counts_original, device_counts_retweets
 
 
-def get_data_dir_name_for(month):
-  """Returns the data directory name for the given month."""
-  year = '2011'
-  if month == '01':
-    year = '2012'
-  return '%s/%s_%s' % (_DATA_DIR, year, month)
-
-
-def is_in_window(date_time):
-  """Checks if the given datetime is within the desired window.
-
-  Keyword Arguments:
-  date_time -- The datetime object to take.
-  
-  Returns:
-  True if within the window, False otherwise.
-  """
-  if (date_time > datetime(year=2011, month=9, day=1) and
-      date_time < datetime(year=2012, month=1, day=1)):
-    return True
-  return False
-
+def find_deltas():
+  """Loads a map of tweet_id to delta values."""
+  log('Finding deltas...')
+  tweet_id_to_delta = {}
+  with open('../data/FolkWisdom/time_deltas.tsv') as in_file:
+    for line in in_file:
+      tokens = line.split('\t')
+      tweet_id = tokens[_TIMEDELTAS_FILE_TWEET_ID_INDEX]
+      # Need to convert to hours
+      delta = int(tokens[_TIMEDELTAS_FILE_DELTA_INDEX]) / 3600
+      tweet_id_to_delta[tweet_id] = delta 
+  return tweet_id_to_delta
+      
 
 def log(message):
   """Helper method to modularize the format of log messages.
@@ -126,28 +147,76 @@ def log(message):
 def run():
   """Main logic for this analysis."""
   FileLog.set_log_dir()
-  all_counts, original_counts, retweet_counts = find_device_counts()
+  Util.ensure_dir_exist(_OUTPUT_DIR)
+  deltas = find_deltas()
+  cache = Util.load_cache()
+  seeds = Util.load_seeds()
+
+  # Do analysis only for stories in top news.
+  param_str = '_t%s' % (int(_SIZE_TOP_NEWS * 100))
+  gts = ground_truths.get_gt_rankings(seeds, DataSet.ALL)
+  top_news = ground_truths.find_target_news(gts, _SIZE_TOP_NEWS)
+  (all_counts, original_counts,
+   retweet_counts) = find_device_counts(sys.maxint, deltas, top_news, cache)
   sorted_all_counts = sorted(all_counts.items(), key=lambda x: x[1][0],
                              reverse=True)
   sorted_original_counts = sorted(original_counts.items(),
                                   key=lambda x: x[1][0], reverse=True)
-  sorted_retweet_counts = sorted(retweet_counts.items(), key=lambda x: x[1][0],
+  sorted_retweet_counts = sorted(retweet_counts.items(),
+                                 key=lambda x: x[1][0],
                                  reverse=True)
 
-  Util.ensure_dir_exist(_OUTPUT_DIR)
-  with open(_OUTPUT_DIR + 'source_device_all.tsv', 'w') as out_file:
+  with open(_OUTPUT_DIR + 'source_device_all%s.tsv' % param_str,
+            'w') as out_file:
     for device, (count, percentage) in sorted_all_counts:
       out_file.write('%s\t%s\t%s\n' % (device, count, percentage))
-  with open(_OUTPUT_DIR + 'source_device_original.tsv', 'w') as out_file:
+  with open(_OUTPUT_DIR + 'source_device_original%s.tsv' % param_str,
+            'w') as out_file:
     for device, (count, percent_of_originals,
                  percent_tweets_original) in sorted_original_counts:
-      out_file.write('%s\t%s\t%s\t%s\n' % (device, count, percent_of_originals,
+      out_file.write('%s\t%s\t%s\t%s\n' % (device, count,
+                                           percent_of_originals,
                                            percent_tweets_original))
-  with open(_OUTPUT_DIR + 'source_device_retweets.tsv', 'w') as out_file:
+  with open(_OUTPUT_DIR + 'source_device_retweets%s.tsv' % param_str,
+            'w') as out_file:
     for device, (count, percent_of_retweets,
                  percent_tweets_retweets) in sorted_retweet_counts:
       out_file.write('%s\t%s\t%s\t%s\n' % (device, count, percent_of_retweets,
-                                           percent_tweets_retweets))
+                                             percent_tweets_retweets))
+
+  # Do analysis w/ delta, including sys.max to do analysis with no delta.
+  for delta in [sys.maxint] + _DELTAS:
+    param_str = '_%s' % delta
+    if delta == sys.maxint:
+      param_str = ''
+
+    (all_counts, original_counts,
+     retweet_counts) = find_device_counts(delta, deltas)
+    sorted_all_counts = sorted(all_counts.items(), key=lambda x: x[1][0],
+                               reverse=True)
+    sorted_original_counts = sorted(original_counts.items(),
+                                    key=lambda x: x[1][0], reverse=True)
+    sorted_retweet_counts = sorted(retweet_counts.items(),
+                                   key=lambda x: x[1][0],
+                                   reverse=True)
+
+    with open(_OUTPUT_DIR + 'source_device_all%s.tsv' % param_str,
+              'w') as out_file:
+      for device, (count, percentage) in sorted_all_counts:
+        out_file.write('%s\t%s\t%s\n' % (device, count, percentage))
+    with open(_OUTPUT_DIR + 'source_device_original%s.tsv' % param_str,
+              'w') as out_file:
+      for device, (count, percent_of_originals,
+                   percent_tweets_original) in sorted_original_counts:
+        out_file.write('%s\t%s\t%s\t%s\n' % (device, count,
+                                             percent_of_originals,
+                                             percent_tweets_original))
+    with open(_OUTPUT_DIR + 'source_device_retweets%s.tsv' % param_str,
+              'w') as out_file:
+      for device, (count, percent_of_retweets,
+                   percent_tweets_retweets) in sorted_retweet_counts:
+        out_file.write('%s\t%s\t%s\t%s\n' % (device, count, percent_of_retweets,
+                                             percent_tweets_retweets))
   log('Analysis complete.')
   
 
